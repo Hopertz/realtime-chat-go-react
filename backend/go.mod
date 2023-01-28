8adc3c6e375bcbecb4ffe7d97887add6a6601782
Hopertz <lugano.ngulwa@gmail.com>
2023-01-28 11:23:03 +0300

    Initial commit

---
+++ backend/go.mod
@@ -0,0 +1,5 @@
+module github.com/hopertz/realtime-chat-go-react
+
+go 1.19
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+import Header from "./components/Header/Header";
+import ChatHistory from "./components/ChatHistory";
+import ChatInput from "./components/ChatInput";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: [],
+    };
+  }
+
+send(event) {
+  if(event.keyCode === 13) {
+    sendMsg(event.target.value);
+    event.target.value = "";
+  }
+}
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message");
+      this.setState((prevState) => ({
+        chatHistory: [...this.state.chatHistory, msg],
+      }));
+      console.log(this.state);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <ChatInput send={this.send} />
+      </div>
+    );
+  }
+}
+
+export default App;
